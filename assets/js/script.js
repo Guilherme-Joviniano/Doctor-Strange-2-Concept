400eab76feed885515cd021dbe567e1e6ddd43d4
GUILHERME JOVINIANO DE SOUSA <22102359@CFP127ALUNO.LOCAL>
2022-04-05 11:25:44 -0300

    fizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 
 
-<<<<<<< HEAD
 const el = document.querySelector("#btn-trailer")
 const videoPlace = document.querySelector("main")
 const content = document.querySelector(".content")
@@ -60,40 +59,6 @@ document.querySelectorAll(".btn-content").forEach((button, index) => {
 
 
 
-=======
-const el = document.querySelector(".button")
-const videoPlace = document.querySelector("main")
-const content = document.querySelector(".content")
-const backButton = document.querySelector("#button-voltar")
-const video = `
-        <iframe width="900px" height="600px" src="https://www.youtube.com/embed/X23XCFgdh2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="button"> <a href="index.html"><span>Voltar</span> </div>
-        `
-
-const animacao = () =>{
-    
-    document.querySelector('.content').classList.add("fade-out")
-    document.querySelector('body').style.backgroundColor = "#000E"
-    
-}
-function resolver2segundos(){
-    return new Promise(animacao =>{
-        setTimeout(()=>{
-            animacao()
-        }, 1000)
-    });
-}
-
-async function abrirTrailer(){
-    //animation
-    await resolver2segundos();
-
-    videoPlace.innerHTML = video
-    
-}
-
-console.log(el)
->>>>>>> 4c759cad5e394b6104e32816cf634bbda664dfea
 el.addEventListener("click", abrirTrailer ,true)
 
 
